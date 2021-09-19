3eb3cdddd142fcf58fd7a57702e58857768adcda
Marcos L <marcosrlj@outlook.com>
2021-09-19 02:38:47 -0300

    styled components
    utilizando proporcoes em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-// import { Text } from 'react-native';
+import { Text } from 'react-native';
 
-import { Container, Title } from './styles';
+import { Container, Header, User, Avatar, TextArea, Greeting, Name } from './styles';
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <User>
+          <Avatar source={{ uri: 'https://avatars.githubusercontent.com/u/34945925?v=4' }} />
+
+          <TextArea>
+            <Greeting>Yo, </Greeting>
+            <Name>Marcos R. Leite Junior</Name>
+          </TextArea>
+        </User>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,55 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(37)}px;
+  /* height: 270px; */
+
+  background-color: ${({ theme }) => theme.colors.one};
+  padding-top: ${RFValue(47)}px;
+`;
+
+export const User = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-left: ${RFValue(19)}px;
+  /* margin-left: 24px; */
+  /* background-color: red; */
+`;
+
+export const Avatar = styled.Image`
+  width: ${RFValue(37)}px;
+  height: ${RFValue(37)}px;
+
+  border-radius: 7px;
+`;
+
+export const TextArea = styled.View`
+  margin-left: 17px;
+`;
+
+export const Greeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const Name = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-size: ${RFValue(15)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
+
+// export const Title = styled.Text`
+//   font-size: 24px;
+//   color: ${({ theme }) => theme.colors.title};
+//   font-family: ${({ theme }) => theme.fonts.bold};
+// `;
