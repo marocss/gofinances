994c47283ec1707ead09c87845e7a9ead73d2e0a
Marcos L <marcosrlj@outlook.com>
2021-09-19 01:42:40 -0300

    styled components
    utilizando fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -2,12 +2,24 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 // import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 import { Dashboard } from './src/screens/Dashboard';
 
 import theme from './src/global/styles/theme';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, 
+    Poppins_500Medium, 
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
