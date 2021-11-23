b89b0e85380f8b89b30fe1427f5bf58b6a4079c9
Marcos L <marcosrlj@outlook.com>
2021-11-23 03:07:07 -0300

    abstraindo configuracoes

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules",
+    "/android",
+    "/ios"
+  ]
+}
